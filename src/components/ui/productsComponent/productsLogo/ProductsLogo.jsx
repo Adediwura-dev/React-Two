18174a746da102b9354708bd27a7235942cc5b47
Bushra <bushragbadegesin@gmail.com>
2026-09-11 22:20:25 +0100

    worked further--

--- src/components/ui/productsComponent/productsLogo/ProductsLogo.jsx
+++ src/components/ui/productsComponent/productsLogo/ProductsLogo.jsx
@@ -1,12 +1,34 @@
 // import "./ProductsLogo.css";
+import Logo1 from "../../../../assets/GoogleProducts/imgi_5_gemini-update.png";
+import Logo2 from "../../../../assets/GoogleProducts/imgi_6_logo-maps-2025-color-2x-web-96dp.webp";
+import Logo3 from "../../../../assets/GoogleProducts/imgi_7_logo-sheets-2026-color-2x-web-64dp (1).webp";
+import Logo4 from "../../../../assets/GoogleProducts/imgi_8_youtube.png";
+import Logo5 from "../../../../assets/GoogleProducts/imgi_2_chrome.png";
+import Logo6 from "../../../../assets/GoogleProducts/imgi_10_logo-gmail-2026-color-2x-web-64dp.webp";
+import Logo7 from "../../../../assets/GoogleProducts/imgi_11_logo-photos-2025-color-2x-web-64dp.webp";
+import Logo8 from "../../../../assets/GoogleProducts/imgi_35_gboard.png";
+import Logo9 from "../../../../assets/GoogleProducts/imgi_47_googlefit.png";
+import Logo10 from "../../../../assets/GoogleProducts/imgi_38_authenticator.png";
+import Logo11 from "../../../../assets/GoogleProducts/imgi_12_android.png";
 
 const ProductsLogo = () => {
   return (
     <div className="products-logo">
-        <h1>Getting Started</h1>
-        <img src="../../assets/GoogleProducts/src/assets/GoogleProducts/imgi_4_logo-docs-2026-color-2x-web-64dp.webp" alt="Image goes " />
+      <div>
+        <img src={Logo1} alt="Image goes here" />
+        <img src={Logo2} alt="Image goes here" />
+        <img src={Logo3} alt="Image goes here" />
+        <img src={Logo4} alt="Image goes here" />
+        <img src={Logo5} alt="Image goes here" />
+        <img src={Logo6} alt="Image goes here" />
+        <img src={Logo7} alt="Image goes here" />
+        <img src={Logo8} alt="Image goes here" />
+        <img src={Logo9} alt="Image goes here" />
+        <img src={Logo10} alt="Image goes here" />
+        <img src={Logo11} alt="Image goes here" />
+      </div>
     </div>
-  )
+  );
 };
 
 export default ProductsLogo;
